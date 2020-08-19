642385026df8888de0ac39a55b6ebdd07234f38a
Krishna Kumar T <krishna.thokala2010@gmail.com>
2020-08-19 08:28:12 +0530

    This works with Dell wireless mouse on centos 7 and the same doesnt work with Keyboard, not sure why | [Krishna]

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,59 @@
 extern crate libusb;
+use libusb::{Device, Context, DeviceHandle};
 use std::time::Duration;
-use std::slice;
+use std::thread;
 
 fn main() {
-    let context = libusb::Context::new().unwrap();
+	let context = libusb::Context::new().unwrap();
+	// Get device by bus num and device id
+	// obtained with lsusb command
+	let selected_device = get_device(&context, 1, 3);
+	if let Some(dev) = selected_device {
+		let mut dev_hdl = dev.open().unwrap();
+		let device_desc = dev.device_descriptor().unwrap();
+		let langs = dev_hdl.read_languages(Duration::from_secs(1)).unwrap();
+		println!("  iProduct             {:3} {}",
+             device_desc.product_string_index().unwrap_or(0),
+			 dev_hdl.read_product_string(langs[0], &device_desc, Duration::from_secs(1)).unwrap_or(String::new()));
+		detach_kernel_driver(&mut dev_hdl);
+		claim_interface(&mut dev_hdl);
+		read_interrupt_data(&mut dev_hdl);
+	}
+}
 
-    for mut device in context.devices().unwrap().iter() {
-        let device_desc = device.device_descriptor().unwrap();
+fn read_interrupt_data(dev_hdl: &mut DeviceHandle) {
+	loop {
+		let mut buf:[u8;256] = [0;256];
+		println!("attempting read from ifc");
+		// 130 is endpoint descriptor address of Dell wireless mouse for 0x82
+		// Endpoint descriptor details can be found with lsusb -v command 
+		match dev_hdl.read_interrupt(130, &mut buf, Duration::from_secs(1)) {
+			Ok(res) => println!("res: {:?}, data: {:?}", res, &buf[..res]),
+			Err(e) => println!("error reading interrupt: {:?}",e)
+		}
+		thread::sleep(Duration::from_millis(1000));
+	}
+}
 
-        println!("Bus {:03} Device {:03} ID {}:{} usb version {:?} manufacturer_string_index {:?}",
-            device.bus_number(),
-            device.address(),
-            device_desc.vendor_id(),
-            device_desc.product_id(),
-        	device_desc.usb_version(),
-        	device_desc.manufacturer_string_index().unwrap());
+fn claim_interface(device_hdl: &mut DeviceHandle) {
+	match device_hdl.claim_interface(1) {
+		Ok(_) => (),
+		Err(e) => println!("error claiming interface: {}", e)
+	}
+}
 
-        let dh = device.open().unwrap();
-        let mut l = dh.read_languages(Duration::new(100,0)).unwrap();
-        println!("device manufacturer string is {:?}", dh.read_manufacturer_string(l.pop().unwrap(),&device_desc,Duration::new(100,0)));
-        // let mut vec: Vec<u8> = Vec::<u8>::with_capacity(64);
-        //     let mut bulk_buf = unsafe {
-        //         slice::from_raw_parts_mut(vec.as_mut_ptr(), vec.capacity())
-        //     };
-        // println!("created bulk_buf");
-        // dh.read_bulk(1,&mut bulk_buf,Duration::new(100,0)).unwrap();
-        // println!("bulk_buf {:?}", bulk_buf);
+fn detach_kernel_driver(device_hdl: &mut DeviceHandle) {
+	match device_hdl.detach_kernel_driver(1) {
+		Ok(_) => (),
+		Err(e) => println!("error detaching kernel driver: {}", e)
+	}
+}
 
-        // let num_configurations = device_desc.num_configurations();
-        // for i in 0..num_configurations {
-	       //  let config_descriptor = device.config_descriptor(i).unwrap();
-	       //  println!("~config_descriptor: {}",i);
-	       //  println!("~~number : {}", config_descriptor.number());
-	       //  println!("~~max_power : {}", config_descriptor.max_power());
-	       //  println!("~~self_powered : {}", config_descriptor.self_powered());
-	       //  println!("~~remote_wakeup : {}", config_descriptor.remote_wakeup());
-	       //  println!("~~description_string_index : {:?}", config_descriptor.description_string_index());
-	       //  let num_interfaces = config_descriptor.num_interfaces();
-	       //  println!("~~num_interfaces : {}", num_interfaces);
-
-	       //  for interface in config_descriptor.interfaces() {
-	       //  	println!("~~Interface: {}", interface.number());
-	       //  	for interface_descriptor in interface.descriptors() {
-	       //  		println!("~~~interface_number {}", interface_descriptor.interface_number());
-	       //  		println!("~~~setting_number {}", interface_descriptor.setting_number());
-	       //  		println!("~~~class_code {}", interface_descriptor.class_code());
-	       //  		println!("~~~sub_class_code {}", interface_descriptor.sub_class_code());
-	       //  		println!("~~~protocol_code {}", interface_descriptor.protocol_code());
-	       //  		println!("~~~description_string_index {:?}", interface_descriptor.description_string_index());
-	       //  		println!("~~~num_endpoints {}", interface_descriptor.num_endpoints());
-
-	       //  		for endpoint_descriptor in interface_descriptor.endpoint_descriptors() {
-	       //  			println!("~~~endpoint: {}", endpoint_descriptor.number());
-	       //  			println!("~~~~address {}", endpoint_descriptor.address());
-	       //  			println!("~~~~Direction {:?}", endpoint_descriptor.direction());
-	       //  			println!("~~~~transfer_type {:?}", endpoint_descriptor.transfer_type());
-	       //  			println!("~~~~sync_type {:?}", endpoint_descriptor.sync_type());
-	       //  			println!("~~~~usage_type {:?}", endpoint_descriptor.usage_type());
-	       //  			println!("~~~~max_packet_size {:?}", endpoint_descriptor.max_packet_size());
-	       //  			println!("~~~~interval {:?}", endpoint_descriptor.interval());
-
-	       //  		}
-	       //  	}
-	       //  }
-        // }
-    }
-
-    // println!("{:?}", context.has_hotplug());
-
-    // let d_h = match context.open_device_with_vid_pid(1008, 15879) {
-    //     Some(device_handle) => device_handle,
-    //     None => panic!("cannot open device")
-    // };
-    // println!("Found device deviceHandle {:?}",d_h.active_configuration().unwrap());
-    // let mut l = d_h.read_languages(Duration::new(100,0)).unwrap();
-    // println!("language is {:?}", d_h.read_string_descriptor(l.pop().unwrap(),0,Duration::new(100,0)));
+fn get_device(context: &Context, bus_num: u8, addr: u8) -> Option<Device>{
+	for device in context.devices().unwrap().iter() {
+		if device.bus_number() == bus_num && device.address() == addr {
+			return Some(device)
+		}
+	}
+	return None
 }
